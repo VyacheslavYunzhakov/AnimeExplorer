7e8205603508e8c10ed83a0c7427dd635c6765e4
Юнжаков Вячеслав <slava-shalala@mail.ru>
2025-05-16 18:12:40 +0300

    ExplorerNavHost inside MainActivity

--- app/src/main/java/com/example/animeexploreranilist/MainActivity.kt
+++ app/src/main/java/com/example/animeexploreranilist/MainActivity.kt
@@ -5,11 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
+import com.example.animeexploreranilist.navigation.ExplorerNavHost
+import com.example.animeexploreranilist.ui.rememberExplorerAppState
 import com.example.animeexploreranilist.ui.theme.AnimeExplorerAnimeListTheme
-import com.example.home.navigation.HomeBaseRoute
-import com.example.home.navigation.homeScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -20,13 +19,7 @@ class MainActivity : ComponentActivity() {
             AnimeExplorerAnimeListTheme {
                 Surface(color = MaterialTheme.colorScheme.background) {
                     val navController = rememberNavController()
-                    // NavGraph sets up the overall navigation for the app.
-                    NavHost(
-                        navController = navController,
-                        startDestination = HomeBaseRoute
-                    ) {
-                        homeScreen {}
-                    }
+                    ExplorerNavHost(rememberExplorerAppState(navController))
                 }
             }
         }
--- app/src/main/java/com/example/animeexploreranilist/navigation/ExplorerNavHost.kt
+++ app/src/main/java/com/example/animeexploreranilist/navigation/ExplorerNavHost.kt
@@ -24,5 +24,6 @@ fun ExplorerNavHost(
                 navController.navigateToLaunchDetails(launchId)*/
             }
         )
+
     }
 }
